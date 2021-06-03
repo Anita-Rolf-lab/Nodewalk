1b29c7314ab30a14217b812c5f8fa8d69faca055
Anita Göndör's Lab <56019767+Anita-Rolf-lab@users.noreply.github.com>
2021-06-03 14:52:59 +0530

    Update Nodewalk_V2.sh

--- Nodewalk_V2.sh
+++ Nodewalk_V2.sh
@@ -34,19 +34,19 @@ if [  -n "$1" ]
 	VAR2="Linuxize"
 	if [ "$1" = "help" ]
 	then
-		echo "\n Use of the Nodewalk pipeline\n\n Mandatory "
+		echo "\n Use of the Nodewalk Pipeline\n\n Mandatory "
 		echo "\t-i : Input fastq.gz directory"
 		echo "\t-a : Reference Genome in .fa extension"
 
 		echo "\n Optional"
-		echo "\t-s : Output Sam directory, Optional"
-		echo "\t-r : Restricted Enzyme"
-		echo "\t-p : Prob defination in .py extension"
+		echo "\t-s : Output Sam Directory, Optional"
+		echo "\t-r : Restriction Enzyme"
+		echo "\t-p : Probe definition in .py extension"
 		echo "\t-o : Output Stats Directory, Optional"	
 			
 		
-		echo "\n\nRun with the detault settings: nw.sh"
-		echo "Detail of folder structure in detault settings"
+		echo "\n\nRun with the default settings: nw.sh"
+		echo "Details of folder structure in default settings"
 		echo "-i: $inputDir"
 		echo "-s: $outputDirSam"
 		echo "-a: $reffa"		
@@ -66,17 +66,17 @@ echo "Input fastq.gz directory: $inputDir"
 echo "Output Sam directory: $outputDirSam"
 echo "Input reference genome .fa : $reffa"
 
-echo "Prob Definition: $pdef"
-echo "Restricted Enzyme : $renzyme"
+echo "Probe Definition: $pdef"
+echo "Restriction Enzyme : $renzyme"
 
 echo "Output Stats Directory :$outputDirStats"
 
 echo "\n\nValidating Provided Directory and files\n\n";
 if [ ! -d "$inputDir" ]; then echo "Error, Directory of input fastq.gz files  '$inputDir' DOES NOT exists.\n";  exit 9999; fi
-if [ ! -d "$outputDirSam" ]; then echo "Error, Directory of output sam files  '$outputDirSam' DOES NOT exists.\n";  exit 9999; fi
+if [ ! -d "$outputDirSam" ]; then echo "Error, Directory of output SAM files  '$outputDirSam' DOES NOT exists.\n";  exit 9999; fi
 if [ ! -d "$outputDirStats" ]; then echo "Error, Directory of output STATs   '$outputDirStats' DOES NOT exists.\n";  exit 9999; fi
 if [ ! -f "$reffa" ]; then echo "Reference genome $reffa does not exist on your filesystem.\n"; exit 9999; fi
-if [ ! -f "$pdef" ]; then echo "Prob definition $pdef does not exist on your filesystem.\n"; exit 9999; fi
+if [ ! -f "$pdef" ]; then echo "Probe definition $pdef does not exist on your filesystem.\n"; exit 9999; fi
 
 
 
@@ -100,7 +100,7 @@ done
 unqpre=$(echo "$var1" | xargs -n1 | sort -u | xargs)
 #echo"${args[@]}"
 echo $unqpre
-echo "total number of Libraries are " ; echo "$unqpre" | wc -w;
+echo "Total number of Libraries are " ; echo "$unqpre" | wc -w;
 
 flag=0
 
